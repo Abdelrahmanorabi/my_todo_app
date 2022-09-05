8e47811754688c7598f1ddb829c4436347a94348
AbdelrahmanDev <abdelrahmanoraby101@gmail.com>
2022-09-06 00:44:16 +0200

    init todo app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      initialRoute: HomePage.rounteName,
+      routes: {HomePage.rounteName: (_) => HomePage()},
+    );
+  }
+}
